1c6be403e011ad077b52c5f784d4973ce7f85497
Justin Hoffman <justin@ohmedia.ca>
2022-11-29 13:30:07 -0600

    add a meta entity

---
+++ src/Entity/Meta.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace OHMedia\MetaBundle\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use OHMedia\FileBundle\Entity\Image;
+
+#[ORM\Entity]
+class Meta
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Image $image = null;
+
+    #[ORM\Column]
+    private ?bool $append_base_title = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?Image
+    {
+        return $this->image;
+    }
+
+    public function setImage(?Image $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function isAppendBaseTitle(): ?bool
+    {
+        return $this->append_base_title;
+    }
+
+    public function setAppendBaseTitle(bool $append_base_title): self
+    {
+        $this->append_base_title = $append_base_title;
+
+        return $this;
+    }
+}
