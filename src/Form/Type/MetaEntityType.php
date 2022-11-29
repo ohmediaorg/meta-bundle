8cbca3a8ef577361b5dfa7d982419d953d711f14
Justin Hoffman <justin@ohmedia.ca>
2022-11-29 16:48:57 -0600

    ability to disable append base title checkbox in form type

--- src/Form/Type/MetaEntityType.php
+++ src/Form/Type/MetaEntityType.php
@@ -6,6 +6,7 @@ use OHMedia\FileBundle\Form\Type\ImageEntityType;
 use OHMedia\MetaBundle\Entity\Meta;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,16 +27,26 @@ class MetaEntityType extends AbstractType
             ->add('image', ImageEntityType::class, [
                 'data' => $meta ? $meta->getImage() : null,
             ])
-            ->add('append_base_title', CheckboxType::class, [
-                'required' => false,
-            ])
         ;
+
+        if ($options['show_append_base_title']) {
+            $builder->add('append_base_title', CheckboxType::class, [
+                'required' => false,
+                'empty_data' => true,
+            ]);
+        }
+        else {
+            $builder->add('append_base_title', HiddenType::class, [
+                'data' => true,
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Meta::class,
+            'show_append_base_title' => true,
         ]);
     }
 }
